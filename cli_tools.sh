257ab216131edf776b3df7c98f64d6df9167e213
Sourav Paul <iamsouravin@gmail.com>
2021-02-25 14:18:50 +0530

    Update kubectl version print

--- cli_tools.sh
+++ cli_tools.sh
@@ -13,7 +13,7 @@ rm -rf awscliv2.zip aws
 curl -LO --silent https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
-kubectl --version
+kubectl version --short --client
 
 # Install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
